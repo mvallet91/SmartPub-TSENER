ad7fe063370b03cf5f7b84017a02b70aaa2a822c
root <root@packer-ubuntu-16.04-server-one>
2018-04-23 11:38:21 +0200

    New indexer with authors for PMC and TUD, added www files

---
+++ tud_download.py
@@ -0,0 +1,68 @@
+import shutil, os.path, pickle, time, requests, sys
+from sickle import Sickle
+
+sickle = Sickle('http://oai.tudelft.nl/ir')
+working_dir = 'data/tudelft_repo/test/'
+filter_types = ['master thesis', 'conference paper', 'doctoral thesis', 'journal article']
+max_size = 100 # In MB
+
+types = []
+items = {}
+failed = []
+downloaded = {}
+x = 0
+large = []
+
+############################ Get list of OAI records ##############################
+
+records = sickle.ListRecords( **{'metadataPrefix': 'oai_dc', 'ignore_deleted': 'True'})
+
+for r in records:
+    types.append(r.metadata['type'][0])
+    uuid = ''
+    uuid = r.metadata['identifier'][0][32:]
+    items[uuid] = r.metadata
+
+print('items', len(items), '- types', len(types))
+
+for dirpath, dirs, files in os.walk(working_dir):
+    for file in files: 
+        path = os.path.join(dirpath, file)
+        uuid = path.split('_')[-1].split('.')[0]
+        downloaded[uuid] = items[uuid]
+
+############################ Download files ##############################
+        
+for uuid in items.keys():
+    download = 'https://repository.tudelft.nl/islandora/object/uuid:' + uuid + '/datastream/OBJ/download'     
+    name = working_dir + 'TUD_' + uuid + '.pdf' 
+    if os.path.exists(name):
+        print('File exists:', name)
+        print('.', end = '')
+        continue
+    r = requests.get(download, stream=True)
+    if r.status_code == 200 and items[uuid]['type'][0] in filter_types:
+        with open(name, 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+        print('Downloaded:', name)
+        x += 1
+    if r.status_code != 200:
+        print('Download failed, status', r.status_code, 'link', download)
+        failed.append(uuid)
+    time.sleep(2)
+    if x == 20:
+        break
+
+############################ Delete large files ##############################
+
+target_size = max_size * 1048514
+
+for dirpath, dirs, files in os.walk(working_dir):
+    for file in files: 
+        path = os.path.join(dirpath, file)
+        if os.stat(path).st_size > target_size:
+            os.remove(path)
+            large.append(file)
+
+print('Deleted large files:', large)
