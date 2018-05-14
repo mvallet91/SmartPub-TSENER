fae9a8e05c00cee3e00622b2d8a947dd13d2a60e
Manuel Valle <mvallet91@gmail.com>
2018-05-14 19:07:20 +0200

    added word2vec to pipeline

--- data_preparation/train_word2vec.py
+++ data_preparation/train_word2vec.py
@@ -5,8 +5,10 @@ import multiprocessing
 import nltk
 import string
 from gensim.models import Word2Vec, Phrases
+from gensim.models.phrases import Phraser
 
-# run: python3 word2vec.py data/dataWord2vec.txt data/modelword2vecbigram.model data/modelword2vecbigram.vec
+
+# run: python3 train_word2vec.py data/dataWord2vec.txt data/modelword2vecbigram.model data/modelword2vecbigram.vec
 
 if __name__ == '__main__':
     program = os.path.basename(sys.argv[0])
@@ -18,22 +20,18 @@ if __name__ == '__main__':
     # check and process input arguments
     if len(sys.argv) < 4:
         sys.exit(1)
-
-    sentence_stream = []
     input_file, output_1, output_2 = sys.argv[1:4]
-    file_sent = open(input_file)
-    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
-    lines = (sent_detector.tokenize(file_sent.read().strip()))
-    bigram_transformer = Phrases(min_count=1, threshold=2)
-    for line in lines:
-        sentence = [word
-                    for word in nltk.word_tokenize(line.lower())
-                    if word not in string.punctuation]
-        sentence_stream.append(sentence)
-        bigram_transformer.add_vocab([sentence])
 
-    model = Word2Vec(bigram_transformer[sentence_stream], size=100, window=2, min_count=2,
-                     workers=multiprocessing.cpu_count(), sg=1)
+    sent_detector = nltk.data.load('tokenizers/punkt/english.pickle')
+    file_sent = open(input_file, 'r', encoding='utf-8')
+    lines = sent_detector.tokenize(file_sent.read())
+    file_sent.close()
+    common_terms = ["of", "with", "without", "and", "or", "the", "a"]
+    sentence_stream = [[w for w in nltk.word_tokenize(line)] for line in lines]
+    bigram_phrases = Phrases(sentence_stream, min_count=2, threshold=2)
+    bigram_transformer = Phraser(bigram_phrases)
+    tokens_ = bigram_transformer[sentence_stream]
+    model = Word2Vec(tokens_, size=100, window=5, min_count=2, workers=multiprocessing.cpu_count(), sg=1)
 
     model.save(output_1)
     model.wv.save_word2vec_format(output_2, binary=False)
